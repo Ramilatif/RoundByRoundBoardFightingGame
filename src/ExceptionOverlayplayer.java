f4d9c8f26f58bcd6c9d11922e272d2032a7aac7a
Ramilatif <122941472+Ramilatif@users.noreply.github.com>
2023-05-22 09:23:47 +0200

    Old

--- src/ExceptionOverlayplayer.java
+++
@@ -1,7 +0,0 @@
-public class ExceptionOverlayplayer extends Exception{
-    public static final long serialVersionUID = 1L;
-
-    public  ExceptionOverlayplayer(){
-        super("Il y a deja un joueur sur cette case");
-    }
-}
